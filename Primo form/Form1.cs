3e0bd6393db974805fd6ae7a436e290d1fbcdaad
Riccardo Camozzi <camozzi.riccardo.studente@itispaleocapa.it>
2023-02-10 13:09:15 +0100

    Aggiungere i file di progetto.

---
+++ Primo form/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Primo_form
+{
+    public partial class Form1 : Form
+    {
+        //dichiarazione variabili
+        public int dim;
+        public string[] array;
+
+        public Form1()
+        {
+            InitializeComponent();
+            array = new string[100];
+            dim = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            aggiunta(textBox1.Text);
+        }
+
+        //funzioi di servizio
+        public void aggiunta(string nome)
+        {
+            array[dim] = nome;
+            dim++;
+        }
+    }
+}
