935b9977772e8cbb87a83ee6f7bd85cf1f16df98
Riccardo Camozzi <camozzi.riccardo.studente@itispaleocapa.it>
2023-02-17 13:30:59 +0100

    Commit finale

--- Primo form/Form1.Designer.cs
+++ Primo form/Form1.Designer.cs
@@ -85,7 +85,7 @@
             // label2
             // 
             this.label2.ForeColor = System.Drawing.SystemColors.Control;
-            this.label2.Location = new System.Drawing.Point(218, 8);
+            this.label2.Location = new System.Drawing.Point(402, 8);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(200, 23);
             this.label2.TabIndex = 4;
@@ -94,7 +94,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(218, 58);
+            this.button2.Location = new System.Drawing.Point(402, 58);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(200, 23);
             this.button2.TabIndex = 5;
@@ -104,7 +104,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(218, 32);
+            this.textBox2.Location = new System.Drawing.Point(402, 32);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(200, 20);
             this.textBox2.TabIndex = 6;
@@ -112,7 +112,7 @@
             // label3
             // 
             this.label3.ForeColor = System.Drawing.SystemColors.Control;
-            this.label3.Location = new System.Drawing.Point(424, 8);
+            this.label3.Location = new System.Drawing.Point(218, 8);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(178, 23);
             this.label3.TabIndex = 7;
@@ -121,14 +121,14 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(427, 32);
+            this.textBox3.Location = new System.Drawing.Point(221, 32);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(175, 20);
             this.textBox3.TabIndex = 8;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(427, 58);
+            this.button3.Location = new System.Drawing.Point(221, 58);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(175, 23);
             this.button3.TabIndex = 9;
@@ -138,7 +138,7 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(613, 59);
+            this.button4.Location = new System.Drawing.Point(611, 58);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(175, 23);
             this.button4.TabIndex = 12;
@@ -148,7 +148,7 @@
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(613, 33);
+            this.textBox4.Location = new System.Drawing.Point(611, 32);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(175, 20);
             this.textBox4.TabIndex = 11;
@@ -156,7 +156,7 @@
             // label4
             // 
             this.label4.ForeColor = System.Drawing.SystemColors.Control;
-            this.label4.Location = new System.Drawing.Point(610, 9);
+            this.label4.Location = new System.Drawing.Point(608, 8);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(178, 23);
             this.label4.TabIndex = 10;
